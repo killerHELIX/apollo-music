83e00c183abc1b08f4b6dd7266c35222932fb583
Ryan Harris <rharris4@mail.umw.edu>
2017-06-15 18:23:21 +0000

    added shared service and now uses a layout controller and individual ones for each page

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,52 @@
 var app = angular.module('app', []);
+var socket = io.connect('https://' + document.domain + ':' + location.port);
 
-app.controller('controller', function($scope, $sce) {
-    var socket = io.connect('https://' + document.domain + ':' + location.port);
+app.service('shared', function() {
+    var loggedIn = false;
+    var currentUser = "";
+    
+    return {
+        isLoggedIn: function () {
+            return loggedIn;
+        },
+        
+        setLoggedIn: function (value) {
+            loggedIn = value;
+        },
+        
+        getCurrentUser: function () {
+            return currentUser;
+        },
+        
+        setCurrentUser: function(value) {
+            currentUser = value;
+        },
+    };
+});
+
+app.controller('owner', function($scope, $sce, shared) {
     
     // required for audio player
     $scope.trustedUrl = function(url) {
         return $sce.trustAsResourceUrl(url);
     };
     
-    $scope.loggedIn = false;
     $scope.playing = false;
     $scope.player = document.getElementById('nowPlaying');
     
     // listen for end of song, move to next in queue
-    $scope.player.addEventListener('ended', function(){
-        var last = $scope.queue.pop();
-        console.log("Song ended.  Now playing... " + last.title);
-        $scope.nowPlaying = last;
-        $scope.play(last);
-        $scope.remove(last);
-        $scope.$apply();
-    });
+    if ($scope.player != null) {
+        $scope.player.addEventListener('ended', function(){
+            var last = $scope.queue.pop();
+            console.log("Song ended.  Now playing... " + last.title);
+            $scope.nowPlaying = last;
+            $scope.play(last);
+            $scope.remove(last);
+            $scope.$apply();
+        });
+    } else {
+        console.log("This page doesn't have an audio player!");
+    }
     
     $scope.selectedTrack = {
         title: "None",
@@ -44,32 +70,17 @@ app.controller('controller', function($scope, $sce) {
     $scope.volume = 1;
     
     socket.on('connect', function() {
-        console.log('connected');
+        console.log('Owner CTRL connected.');
         console.log('$scope.nowPlaying.url: ' + $scope.nowPlaying.url);
         console.log("path: " + $scope.nowPlayingSrc);
         console.log("audio path: " + document.getElementById('nowPlaying').src);
 
     });
     
-    // changes loggedIn to true
-    $scope.login = function(username, password) {
-        console.log("Entered $scope.login in app.js");
-        $scope.currentUser = username;
-        $scope.loggedIn = true;
-        console.log("Received " + username + " and " + password);
-        console.log("$scope.username: " + $scope.username + ", $scope.password: " + $scope.password);
-        
-    };
     
-    // changes loggedIn to false
-    $scope.logout = function() {
-        console.log("Entered $scope.logout in app.js");
-        $scope.loggedIn = false;
-        console.log("Received " + $scope.username + " and " + $scope.password);
-    };
     
     $scope.selectTrack = function($event){
-        console.log("Entered $scope.selectTrack in app.js");
+        console.log("Entered $scope.selectTrack in owner CTRL (app.js)");
         var element = event.target;
         var id = element.id;
         console.log("Received " + id);
@@ -81,7 +92,7 @@ app.controller('controller', function($scope, $sce) {
     // pushes selected track to queue
     // TODO: Actually query a database instead of an incredibly tedious switch statement
     $scope.push = function() {
-        console.log("Entered $scope.addToQueue on scope.js");
+        console.log("Entered $scope.addToQueue on owner CTRL (app.js)");
         switch($scope.selectedTrack.title){
             case 'A3':
                 $scope.queue.push({
@@ -134,7 +145,7 @@ app.controller('controller', function($scope, $sce) {
     
     // remove specific track from queue
     $scope.remove = function(track) {
-        console.log("Entered $scope.remove in app.js");
+        console.log("Entered $scope.remove in owner CTRL (app.js)");
         for (let i = 0; i < $scope.queue.length; i++) {
             if ($scope.queue[i].id == track.id) {
                 console.log("Found matching IDs: " + $scope.queue[i].id + ", " + track.id);
@@ -152,7 +163,7 @@ app.controller('controller', function($scope, $sce) {
     
     // Plays selected track without removing from the queue
     $scope.play = function(track){
-        console.log("Entered $scope.play on app.js, params: " + track.title + ", " + track.genre + ", " + track.url + ", " + track.id);
+        console.log("Entered $scope.play on owner CTRL (app.js), params: " + track.title + ", " + track.genre + ", " + track.url + ", " + track.id);
         $scope.nowPlaying = track;
         
         // load new url to player
@@ -177,4 +188,51 @@ app.controller('controller', function($scope, $sce) {
             return "btn btn-default btn-lg fa fa-pause";
         }
     };
+    
+});
+
+app.controller('layout', function($scope, $sce, shared) {
+    $scope.loggedIn = shared.isLoggedIn();
+    $scope.currentUser = shared.getCurrentUser();
+    
+    socket.on('connect', function() {
+        console.log("Layout CTRL connected.");
+        console.log("shared.loggedIn: " + shared.isLoggedIn());
+        console.log("$scope.loggedIn: " + $scope.loggedIn);
+
+    });
+    
+    // wrapper to guarantee value integrity
+    $scope.setLoggedIn = function (value) {
+        $scope.loggedIn = value;
+        shared.setLoggedIn(value);
+    }
+    
+    $scope.setCurrentUser = function (value) {
+        $scope.currentUser = value;
+        shared.setCurrentUser(value);
+    }
+   
+    // changes loggedIn to true
+    $scope.login = function(username, password) {
+        console.log("Entered $scope.login in layout CTRL (app.js)");
+        $scope.setCurrentUser(username);
+        $scope.setLoggedIn(true);
+        console.log("Received " + username + " and " + password);
+        console.log("$scope.username: " + $scope.username + ", $scope.password: " + $scope.password);
+        console.log("shared.loggedIn: " + shared.isLoggedIn());
+        console.log("$scope.loggedIn: " + $scope.loggedIn);
+        
+    };
+    
+    // changes loggedIn to false
+    $scope.logout = function() {
+        console.log("Entered $scope.logout in layout CTRL (app.js)");
+        $scope.setLoggedIn(false);
+        console.log("Received " + $scope.username + " and " + $scope.password);
+        console.log("shared.loggedIn: " + shared.isLoggedIn());
+        console.log("$scope.loggedIn: " + $scope.loggedIn);
+
+    };
+   
 });
